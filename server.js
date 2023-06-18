5eed5d9cc60bee900e5b53571b6d369c30ba78fd
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-06-18 13:27:20 -0400

    dehls - testing falcoln

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/prompt', (req, res) => {
 app.post('/ask', async (req, res) => {
     const userInput = req.body.prompt;
     const { generated_text } = await inference.textGeneration({
-        model: 'distilgpt2',
+        model: 'tiiuae/falcon-7b-instruct',
         inputs: userInput,
         max_length: 1000
     });
