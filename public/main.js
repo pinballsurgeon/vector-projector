a3f8d4922a58f5b153f29cdc1610c3e0dc921a59
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-06-17 21:37:15 -0400

    dehls - update for llm

--- public/main.js
+++ public/main.js
@@ -7,3 +7,20 @@ d3.select("#my_dataviz")
   .attr("cy", 250)
   .attr("r", 50)
   .attr("fill", "blue");
+
+async function askGPT() {
+    const userInput = document.getElementById('userInput').value;
+
+    const response = await fetch('/ask', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: userInput }),
+    });
+
+    const data = await response.json();
+    document.getElementById('gptResponse').innerText = data.response;
+}
+
+document.getElementById('askButton').addEventListener('click', askGPT);
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
+const { HfInference } = require("@huggingface/inference");
+
 const app = express();
+const inference = new HfInference("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");
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.post('/ask', async (req, res) => {
+    const userInput = req.body.prompt;
+    const { generated_text } = await inference.textGeneration({
+        model: 'gpt2',
+        inputs: userInput
+    });
+    res.json({ response: generated_text });
 });
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
