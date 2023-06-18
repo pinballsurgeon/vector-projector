0ab974d5e537850161db8afef1e1a451c240dd96
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-06-18 10:27:24 -0400

    dehls - expanding prompt prefixes

--- public/main.js
+++ public/main.js
@@ -8,21 +8,29 @@ d3.select("#my_dataviz")
   .attr("r", 50)
   .attr("fill", "blue");
 
+async function getPrompt() {
+    const response = await fetch('/prompt');
+    const data = await response.json();
+    return data.prompt;
+}
+
 async function askGPT() {
     const userInput = document.getElementById('userInput').value;
+
+    const prompt = await getPrompt();
+    const fullPrompt = prompt.replace('<USERINPUT TOPIC>', userInput);
 
     const response = await fetch('/ask', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ prompt: userInput }),
+        body: JSON.stringify({ prompt: fullPrompt }),
     });
 
     const data = await response.json();
-    let responseText = data.response.replace(userInput, ''); // Remove the userInput from the response
+    let responseText = data.response.replace(fullPrompt, ''); // Remove the fullPrompt from the response
 
-    // Preprocessing the responseText into a list
     responseText = responseText.trim().replace(/\[|\]|'/g, ""); // Remove brackets and quotes
     let responseList = responseText.split(","); // Split into array by comma
     responseList = responseList.map(item => item.trim()); // Remove any leading/trailing spaces in each item
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const path = require('path');
 const { HfInference } = require("@huggingface/inference");
+const fs = require('fs');
 
 const app = express();
 const inference = new HfInference("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");
 
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/prompt', (req, res) => {
+    fs.readFile('listPrompts.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(500);
+        } else {
+            const prompts = JSON.parse(data);
+            res.json({ prompt: prompts[0] });
+        }
+    });
+});
 
 app.post('/ask', async (req, res) => {
     const userInput = req.body.prompt;
@@ -17,7 +30,6 @@ app.post('/ask', async (req, res) => {
     });
     res.json({ response: generated_text });
 });
-
 
 const port = process.env.PORT || 3000;
 
