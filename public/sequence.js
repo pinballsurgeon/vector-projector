9964d492a6fd4f2a4ef684b6e4663db985f4bede
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-07-02 17:19:06 -0400

    dehls - updating distinguishing features 11

--- public/sequence.js
+++ public/sequence.js
@@ -23,50 +23,6 @@ export const combineAndCleanList = (initialList, expandedList) => {
     return combinedList.filter(item => item !== '');
   };
 
-// A function to fetch a list from the Language Learning Model (LLM) given a promptKey and user input
-export const fetchListFromLLM = async (promptKey, userInput) => {
-    try {
-      const prompt = await getPrompt(promptKey);
-      const fullPrompt = prompt.replace('<USERINPUT TOPIC>', userInput);
-      appendLog(`Full prompt: ${fullPrompt}`);
-  
-      const { model, temperature, top_p, num_return_sequences } = getModelAndParams();
-      appendLog(`Selected model: ${model}`);
-      appendLog(`Selected temperature: ${temperature}`);
-      appendLog(`Selected top_p: ${top_p}`);
-      appendLog(`Selected num_return_sequences: ${num_return_sequences}`);
-  
-      // SEND PROMPT
-      appendLog('Sending request to /ask...');
-      const response = await fetch('/ask', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ 
-              prompt: fullPrompt, 
-              model: model,
-              temperature: temperature,
-              top_p: top_p,
-              num_return_sequences: num_return_sequences
-          }),
-      });
-  
-      // We need to make sure the response is OK before we can parse it as JSON
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${JSON.stringify(response)}`);
-      }
-
-      // clean prompt results
-      const data = await response.json(); // Parsing the response data as JSON
-      const cleanedResponse = cleanResponse(data.response, fullPrompt);
-      appendLog(`Cleaned response: ${cleanedResponse}`);
-
-      // RETURN CLEANED RESPONSE
-      return cleanedResponse;
-
-    } catch (error) {
-      appendLog(`Error during list generation: ${error}`);
-    }
-};
 
 // A function to generate and expand a list based on user input
 export const listPerpetuator = async () => {
