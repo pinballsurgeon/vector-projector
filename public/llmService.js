1eeb84fbcd031b1e839b6af0e4d057202c9787b9
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-07-04 22:56:46 -0400

    dehls - vectorizer data json 3

--- public/llmService.js
+++ public/llmService.js
@@ -8,45 +8,56 @@ export const cleanResponse = (responseText, fullPrompt) => {
 };
 
 // A function to fetch a list from the Language Learning Model (LLM) given a promptKey and user input
-export const fetchListFromLLM = async (prompt, userInput) => {
+export const fetchListFromLLM = async (promptKey, userInput, item = null, attribute = null) => {
     try {
-      // const fullPrompt = prompt.replace('<USERINPUT TOPIC>', userInput);
-      // appendLog(`Full prompt: ${fullPrompt}`);
-      const fullPrompt = listPrompts[prompt].replace('<USERINPUT TOPIC>', userInput);
-      appendLog(`Full prompt: ${fullPrompt}`);
-  
-      const { model, temperature, top_p, num_return_sequences } = getModelAndParams();
-      appendLog(`Selected model: ${model}`);
-      appendLog(`Selected temperature: ${temperature}`);
-      appendLog(`Selected top_p: ${top_p}`);
-      appendLog(`Selected num_return_sequences: ${num_return_sequences}`);
-  
-      // SEND PROMPT
-      appendLog('Sending request to /ask...');
-      const response = await fetch('/ask', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ 
-              prompt: fullPrompt, 
-              model: model,
-              temperature: temperature,
-              top_p: top_p,
-              num_return_sequences: num_return_sequences
-          }),
-      });
-  
-      // We need to make sure the response is OK before we can parse it as JSON
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${JSON.stringify(response)}`);
-      }
+        let prompt = listPrompts[promptKey];
+        if (item) {
+            prompt = prompt.replace("{item}", item);
+        }
+        if (attribute) {
+            prompt = prompt.replace("{attribute}", attribute);
+        }
+        
+        const modelAndParams = getModelAndParams();
+        
+        appendLog(`Full prompt: ${prompt}`);
 
-      // clean prompt results
-      const data = await response.json(); // Parsing the response data as JSON
-      const cleanedResponse = cleanResponse(data.response, fullPrompt);
-      appendLog(`Cleaned response: ${cleanedResponse}`);
 
-      // RETURN CLEANED RESPONSE
-      return cleanedResponse;
+        const fullPrompt = listPrompts[prompt].replace('<USERINPUT TOPIC>', userInput);
+        appendLog(`Full prompt: ${fullPrompt}`);
+    
+        const { model, temperature, top_p, num_return_sequences } = getModelAndParams();
+        appendLog(`Selected model: ${model}`);
+        //   appendLog(`Selected temperature: ${temperature}`);
+        //   appendLog(`Selected top_p: ${top_p}`);
+        //   appendLog(`Selected num_return_sequences: ${num_return_sequences}`);
+    
+        // SEND PROMPT
+        appendLog('Sending request to /ask...');
+        const response = await fetch('/ask', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                prompt: fullPrompt, 
+                model: model,
+                temperature: temperature,
+                top_p: top_p,
+                num_return_sequences: num_return_sequences
+            }),
+        });
+    
+        // We need to make sure the response is OK before we can parse it as JSON
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${JSON.stringify(response)}`);
+        }
+
+        // clean prompt results
+        const data = await response.json(); // Parsing the response data as JSON
+        const cleanedResponse = cleanResponse(data.response, fullPrompt);
+        appendLog(`Cleaned response: ${cleanedResponse}`);
+
+        // RETURN CLEANED RESPONSE
+        return cleanedResponse;
 
     } catch (error) {
       appendLog(`Error during list generation: ${error}`);
