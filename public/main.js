37be46e9d636411cacd399bfcc1669866fce9a1c
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-07-15 13:31:11 -0400

    dehls - champions 23

--- public/main.js
+++ public/main.js
@@ -32,24 +32,6 @@ d3.select("#my_dataviz")
     initializeModelParams();            // Fetch model parameters on page load
     initializePrompts();                // Fetch prompts on page load
 
-    
-    // // param - TEMPERATURE
-    // document.getElementById('temperature').addEventListener('input', (event) => {
-    //     selectedTemperature = event.target.value;
-    // });
-
-    // // param - TOP P
-    // document.getElementById('top_p').addEventListener('input', (event) => {
-    //     selectedTopP = event.target.value;
-    // });
-
-    // // param - RETURN SEQUENCES
-    // document.querySelectorAll('input[name="num_return_sequences"]').forEach((element) => {
-    //     element.addEventListener('change', (event) => {
-    //         selectedNumSequences = event.target.value;
-    //     });
-    // });
-
 });
 
 // TOPIC HANDLER
@@ -60,12 +42,61 @@ document.getElementById('askButton').addEventListener('click', async () => {
 // DIFFERENTIATING ATTRIBUTES
 document.getElementById('listButton').addEventListener('click', differentiatingTopicsGenerator);
 
-// New event listener for 'vectorizeButton'
+// // New event listener for 'vectorizeButton'
+// document.getElementById('vectorizeButton').addEventListener('click', async () => {
+//     const ratings = await generateRatings();
+//     // const ratings = await generateRange();
+
+//     // Display the ratings in 'llmRatings' div
+//     document.getElementById('llmRatings').innerText = JSON.stringify(ratings, null, 2);
+//     // document.getElementById('llmRatings').innerText = ratings;
+// });
+
+// Event listener for 'vectorizeButton'
 document.getElementById('vectorizeButton').addEventListener('click', async () => {
     const ratings = await generateRatings();
-    // const ratings = await generateRange();
 
     // Display the ratings in 'llmRatings' div
     document.getElementById('llmRatings').innerText = JSON.stringify(ratings, null, 2);
-    // document.getElementById('llmRatings').innerText = ratings;
+
+    // Show the 'PCA' button
+    document.getElementById('pcaButton').style.display = 'block';
+});
+
+
+// PCA function
+function performPCA(data) {
+    // Transform your data into an array of values
+    let items = Object.keys(data);
+    let values = items.map(item => Object.values(data[item]));
+
+    // Perform PCA
+    let pca = new PCA(values);
+
+    // Get the three first principal components
+    let components = pca.predict(values, { nComponents: 3 });
+
+    // Convert the result back into the original format
+    let result = items.reduce((res, item, index) => {
+        res[item] = {
+            component1: components[index][0],
+            component2: components[index][1],
+            component3: components[index][2],
+        };
+        return res;
+    }, {});
+
+    return result;
+}
+
+// Event listener for 'PCA' button
+document.getElementById('pcaButton').addEventListener('click', () => {
+    // Get the ratings from 'llmRatings' div
+    let ratings = JSON.parse(document.getElementById('llmRatings').innerText);
+
+    // Perform PCA
+    let pcaResult = performPCA(ratings);
+
+    // Replace the content in 'llmRatings' div with the PCA result
+    document.getElementById('llmRatings').innerText = JSON.stringify(pcaResult, null, 2);
 });
