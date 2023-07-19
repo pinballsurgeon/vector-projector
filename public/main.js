5bb910f3852483414251619774fc12bc8e4d7151
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-07-19 17:29:51 -0400

    dehls - champions 30

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 //CLIENT
-import {updateSidebar, initializeModels, initializeModelParams, initializePrompts} from './sidebar.js';
+import {updateSidebar, initializeModels, initializeModelParams, initializePrompts, appendLog} from './sidebar.js';
 import { differentiatingTopicsGenerator } from './attributeGenerator.js';
 import { listPerpetuator } from './listPerpetuator.js';
 import { generateRatings, generateRange } from './ratingGenerator.js';
@@ -59,13 +59,15 @@ document.getElementById('pcaButton').addEventListener('click', async () => {
     // Get the ratings from 'llmRatings' div
     let ratings = JSON.parse(document.getElementById('llmRatings').innerText);
 
+    appendLog(`PCA json - ${JSON.stringify(ratings)}`);
+
     // Send ratings data to server for PCA
     const response = await fetch('/performPCA', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(ratings)
+        body: ratings
     });
 
     const pcaResult = await response.json();
@@ -73,7 +75,5 @@ document.getElementById('pcaButton').addEventListener('click', async () => {
     // Replace the content in 'llmRatings' div with the PCA result
     document.getElementById('llmRatings').innerText = JSON.stringify(pcaResult, null, 2);
 
-    // Write the PCA result into 'llm3dVector' div
-    document.getElementById('llm3dVector').innerText = JSON.stringify(pcaResult, null, 2);
 });
 
