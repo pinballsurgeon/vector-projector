9dfee244cfc7674d075ebb5b44520e1ff73c4e18
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-07-30 15:41:41 -0400

    dehls - cube work 13

--- public/main.js
+++ public/main.js
@@ -7,10 +7,14 @@ import { createOrUpdateCube } from './cubeManager.js';
 // Create scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+camera.position.z = 20;
+
+
 
 // Attach renderer to 'my_dataviz' div
 document.getElementById('my_dataviz').appendChild(renderer.domElement);
