b0cc9705754ac9fbc06671004dde517c55b15f40
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-07-30 21:34:44 -0400

    dehls - wee waa 3

--- public/cubeManager.js
+++ public/cubeManager.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+// import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import { appendLog, getModelAndParams, listPrompts } from './sidebar.js';
