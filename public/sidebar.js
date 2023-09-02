c5443347c461204b945409b75d85c712f706971e
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-09-02 13:28:00 -0400

    dehls - cube stats 14

--- public/sidebar.js
+++ public/sidebar.js
@@ -211,6 +211,7 @@ export function setCubeImageInSidebar(imageUrl, itemName, originalRatings, cubes
 // Helper function to calculate average ratings
 function calculateAverageRatingsExceptFor(itemName, cubes) {
     
+    appendLog(`calculateAverageRatingsExceptFor item - ${itemName}, all_cbues: ${cubes}`);
     const attributes = Object.keys(cubes[itemName].originalRatings);
     let summedRatings = {};
     let totalCount = Object.keys(cubes).length - 1; // minus the selected cube
