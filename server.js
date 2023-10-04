e0ba4f0b65405ceeb462e68ba7c69709145146fc
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-10-03 21:54:10 -0400

    dehls - data store 28

--- server.js
+++ server.js
@@ -136,6 +136,29 @@ app.get('/prompt/:promptKey', (req, res, next) => {
     });
 });
 
+
+// // This code is for v4 of the openai package: npmjs.com/package/openai
+// import OpenAI from "openai";
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const response = await openai.chat.completions.create({
+//   model: "gpt-3.5-turbo",
+//   messages: [
+//     {
+//       "role": "user",
+//       "content": ""
+//     }
+//   ],
+//   temperature: 1,
+//   max_tokens: 256,
+//   top_p: 1,
+//   frequency_penalty: 0,
+//   presence_penalty: 0,
+// });
+
 app.post('/ask', async (req, res, next) => {
     try {
         const userInput = req.body.prompt;
@@ -144,11 +167,27 @@ app.post('/ask', async (req, res, next) => {
 
         // If model is GPT-3, call OpenAI's API
         if (model === 'gpt-3') {
-            const gptResponse = await openai.createCompletion({
-                model: "gpt-4",
-                prompt: userInput,
-                max_tokens: 200
-            });
+
+        const gptResponse = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: userInput
+            }
+          ],
+          temperature: 1,
+          max_tokens: 256,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        });
+
+            // const gptResponse = await openai.createCompletion({
+            //     model: "gpt-4",
+            //     prompt: userInput,
+            //     max_tokens: 200
+            // });
 
             res.json({ response: gptResponse.data.choices[0].text.trim() });
       
