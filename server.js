c072086708be9a1382bb9fb45e0b97e7f1aa582c
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-11-07 14:14:22 -0500

    dehls - new gpt4 turbo wow

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.post('/ask', async (req, res, next) => {
             res.json({ response: gptResponse.data.choices[0].text.trim() });
 
         // OpenAI - Create Chat Completion 
-        } else if (['gpt-3.5-turbo', 'gpt-4'].includes(model)) {
+        } else if (['gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview'].includes(model)) {
 
             const gptResponse = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
