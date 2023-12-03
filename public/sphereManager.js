7716826e17bb1a61ba2574312304339a644ef4c9
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-12-03 08:38:53 -0500

    dehls - hammer time

--- public/sphereManager.js
+++ public/sphereManager.js
@@ -1,10 +1,21 @@
-// import * as THREE from 'three';
-import { appendLog} from './sidebar.js';
+
+import { appendLog } from './sidebar.js';
 import { cubes, scene } from './cubeManager.js';
 
-// This function will check for closeness between all cubes and create spheres around those that are close
-export const encaseCubesInSpheres = (cubes, scene, threshold = 0.5) => {
+// Global array to track spheres
+let spheres = [];
 
+// Function to clear all spheres from the scene
+const clearSpheres = () => {
+    spheres.forEach(sphere => {
+        scene.remove(sphere);
+    });
+    spheres = [];
+};
+
+export const encaseCubesInSpheres = (cubes, scene, threshold = 0.5) => {
+    // Clear existing spheres first
+    clearSpheres();
 
     appendLog(`SPHERES HERES`);
     const sphereMaterial = new THREE.MeshBasicMaterial({
@@ -13,37 +24,13 @@ export const encaseCubesInSpheres = (cubes, scene, threshold = 0.5) => {
         opacity: 0.5
     });
 
-    // Function to calculate the distance between two points in 3D space
-    const distance = (cube1, cube2) => {
-        const dx = cube1.position.x - cube2.position.x;
-        const dy = cube1.position.y - cube2.position.y;
-        const dz = cube1.position.z - cube2.position.z;
-        return Math.sqrt(dx * dx + dy * dy + dz * dz);
-    };
+    // ... [rest of your encaseCubesInSpheres function] ...
 
-    // Iterate over each cube and check for closeness with all other cubes
-    cubes.forEach((cube1, index) => {
-        cubes.slice(index + 1).forEach((cube2) => {
-            if (distance(cube1, cube2) < threshold) {
-                // Calculate the midpoint between the two cubes
-                const midX = (cube1.position.x + cube2.position.x) / 2;
-                const midY = (cube1.position.y + cube2.position.y) / 2;
-                const midZ = (cube1.position.z + cube2.position.z) / 2;
-
-                // Create a sphere at the midpoint with a radius slightly larger than the threshold
-                const sphereGeometry = new THREE.SphereGeometry(threshold * 1.1, 32, 32);
-                const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-                sphere.position.set(midX, midY, midZ);
-                scene.add(sphere);
-            }
-        });
-    });
+    // After creating a sphere, add it to the spheres array
+    spheres.push(sphere);
 };
 
-
 export const updateSpheres = (newThreshold) => {
-
     // Encase cubes in new spheres with updated threshold
     encaseCubesInSpheres(cubes, scene, newThreshold);
-  };
+};
