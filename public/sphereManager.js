13edd411392c7132cab1dfd3c1c8d43fdb5153f2
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-12-08 22:33:47 -0500

    dehls - wow now

--- public/sphereManager.js
+++ public/sphereManager.js
@@ -15,45 +15,110 @@ const clearSpheres = () => {
     spheres.length = 0; // Clear the array
 };
 
-// This function will check for closeness between all cubes and create spheres around those that are close
-export const encaseCubesInSpheres = (cubes, scene, threshold = 0.5) => {
-    clearSpheres(); // Clear previous spheres first
-    const sphereMaterial = new THREE.MeshBasicMaterial({
-        color: 0x00ff00, // green color
-        transparent: true,
-        opacity: 0.5
+const canAddSphere = (newSphere, maxOverlapPercentage) => {
+    for (let existingSphere of spheres) {
+        if (isOverlapTooMuch(newSphere, existingSphere, maxOverlapPercentage)) {
+            return false; // Do not add the new sphere if overlap is too much
+        }
+    }
+    return true; // Add the sphere if all is good
+};
+
+const isOverlapTooMuch = (sphere1, sphere2, maxOverlapPercentage) => {
+    const distanceBetweenCenters = sphere1.position.distanceTo(sphere2.position);
+    const radiiSum = sphere1.geometry.parameters.radius + sphere2.geometry.parameters.radius;
+    const allowedOverlapDistance = radiiSum * (1 - (maxOverlapPercentage / 100));
+
+    return distanceBetweenCenters < allowedOverlapDistance;
+};
+
+
+const calculateCentroid = (group) => {
+    let sumX = 0, sumY = 0, sumZ = 0;
+    group.forEach(cube => {
+        sumX += cube.position.x;
+        sumY += cube.position.y;
+        sumZ += cube.position.z;
     });
-
-    // Function to calculate the distance between two points in 3D space
-    const distance = (cube1, cube2) => {
-        const dx = cube1.position.x - cube2.position.x;
-        const dy = cube1.position.y - cube2.position.y;
-        const dz = cube1.position.z - cube2.position.z;
-        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    return {
+        x: sumX / group.length,
+        y: sumY / group.length,
+        z: sumZ / group.length
     };
+};
 
-    // Iterate over each cube and check for closeness with all other cubes
-    cubes.forEach((cube1, index) => {
-        cubes.slice(index + 1).forEach((cube2) => {
-            if (distance(cube1, cube2) < threshold) {
-                // Calculate the midpoint between the two cubes
-                const midX = (cube1.position.x + cube2.position.x) / 2;
-                const midY = (cube1.position.y + cube2.position.y) / 2;
-                const midZ = (cube1.position.z + cube2.position.z) / 2;
 
-                // Create a sphere at the midpoint with a radius slightly larger than the threshold
-                const sphereGeometry = new THREE.SphereGeometry(threshold * 1.1, 32, 32);
-                const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+const groupCubes = (cubes, threshold) => {
+    let groups = [];
+    let visited = new Set();
 
-                sphere.position.set(midX, midY, midZ);
-                scene.add(sphere);
+    // Helper function to find all cubes close to the given cube
+    const findCloseCubes = (cube, group) => {
+        visited.add(cube);
+        group.push(cube);
 
-                // add to spheres
-                spheres.push(sphere);
+        // Check closeness with all other cubes
+        cubes.forEach(otherCube => {
+            if (!visited.has(otherCube)) {
+                const distanceBetweenCubes = cube.position.distanceTo(otherCube.position);
+                if (distanceBetweenCubes < threshold) {
+                    findCloseCubes(otherCube, group); // Recursive call
+                }
             }
         });
+    };
+
+    // Iterate over each cube and group them
+    cubes.forEach(cube => {
+        if (!visited.has(cube)) {
+            let group = [];
+            findCloseCubes(cube, group); // Find all close cubes recursively
+            groups.push(group); // Add this group to the list of groups
+        }
+    });
+
+    return groups;
+};
+
+// Distance calculation method for THREE.Vector3
+THREE.Vector3.prototype.distanceTo = function (v) {
+    const dx = this.x - v.x, dy = this.y - v.y, dz = this.z - v.z;
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+};
+
+
+// This function will check for closeness between all cubes and create spheres around those that are close
+// export const encaseCubesInSpheres = (cubes, scene, threshold = 0.5) => {
+export const encaseCubesInSpheres = (cubes, scene, threshold = 0.5, minCubesPerSphere = 1, maxOverlapPercentage = 100) => {
+    clearSpheres(); // Clear previous spheres first
+
+    const groups = groupCubes(cubes, threshold); // Use the groupCubes function to group cubes
+    groups.forEach(group => {
+        // Only create spheres for groups with enough cubes
+        if (group.length >= minCubesPerSphere) {
+            const sphereGeometry = new THREE.SphereGeometry(threshold * 1.1, 32, 32);
+            const sphereMaterial = new THREE.MeshBasicMaterial({
+                color: 0x00ff00,
+                transparent: true,
+                opacity: 0.5
+            });
+
+            // Calculate the centroid of the group for sphere positioning
+            const centroid = calculateCentroid(group);
+            const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+            sphere.position.set(centroid.x, centroid.y, centroid.z);
+
+            // Now check for overlap before adding the sphere
+            if (canAddSphere(sphere, maxOverlapPercentage)) {
+                scene.add(sphere);
+                spheres.push(sphere); // Track the sphere
+            }
+        }
     });
 };
+    
+
+
 
 
 export const updateSpheres = (newThreshold) => {
