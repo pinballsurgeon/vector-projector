fd3bc2510cc6b6b5e2300c89f7a9d28283e97b72
Dan Ehlers <dan.ehlers1@accesscfa.com>
2023-12-23 20:40:51 -0500

    dehls - wow now

--- public/main.js
+++ public/main.js
@@ -104,3 +104,31 @@ document.getElementById('overlapSlider').addEventListener('input', function() {
   
   updateSpheres(thresholdValue, minCubesValue, overlapValue);
 });
+
+
+function openModelTab(evt, modelName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(modelName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  // Load or update the canvas for the selected model
+  updateCanvasForModel(modelName);
+}
+
+// Initial tab open (default to GPT-4)
+openModelTab(event, 'GPT-4');
