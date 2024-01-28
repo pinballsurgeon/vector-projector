f4604b52dae023d7f6f0fe1dc87c3f0fe7ca12f8
pinballsurgeon <pinballsurgeon@gmail.com>
2024-01-28 15:12:22 -0500

    dehls - remvoing old sidebar selection

--- public/main.js
+++ public/main.js
@@ -118,11 +118,11 @@ async function openModelTab(evt, modelName) {
     //     tabcontent[i].style.display = "none";
     // }
 
-    // // Get all elements with class="tablinks" and remove the class "active"
-    // tablinks = document.getElementsByClassName("tablinks");
-    // for (i = 0; i < tablinks.length; i++) {
-    //     tablinks[i].className = tablinks[i].className.replace(" active", "");
-    // }
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
     // // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(modelName).style.display = "block";
