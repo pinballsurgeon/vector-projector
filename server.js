766c404ea6278a2981ec7f5bec4903da4c1db8af
pinballsurgeon <pinballsurgeon@gmail.com>
2024-01-29 22:05:49 -0500

    dehls - remvoing old sidebar selection

--- server.js
+++ server.js
@@ -245,8 +245,9 @@ app.listen(port, () => {
 );
 
 
-app.use(bodyParser.json());  // This middleware parses incoming requests with JSON payloads
-
+// app.use(bodyParser.json());  // This middleware parses incoming requests with JSON payloads
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 
 app.post('/vector_db', async (req, res) => {
