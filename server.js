ea1affd76f904cda90c3c38649dc07498cba39c7
pinballsurgeon <pinballsurgeon@gmail.com>
2024-02-24 15:00:31 -0500

    dehls - yay

--- server.js
+++ server.js
@@ -319,19 +319,19 @@ async function gemini_generateContent(prompt) {
       safety_settings: [
               {
                   "category": "HARM_CATEGORY_HATE_SPEECH",
-                  "threshold": "BLOCK_NONE"
+                  "threshold": "BLOCK_FEW"
               },
               {
                   "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                  "threshold": "BLOCK_NONE"
+                  "threshold": "BLOCK_FEW"
               },
               {
                   "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                  "threshold": "BLOCK_NONE"
+                  "threshold": "BLOCK_FEW"
               },
               {
                   "category": "HARM_CATEGORY_HARASSMENT",
-                  "threshold": "BLOCK_NONE"
+                  "threshold": "BLOCK_FEW"
               }
           ]
     });
