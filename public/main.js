483d03275efb5503005e818237039dd52cfc059a
pinballsurgeon <pinballsurgeon@gmail.com>
2024-03-20 20:58:09 -0400

    dehls - weeeee

--- public/main.js
+++ public/main.js
@@ -628,7 +628,7 @@ function adjustCanvasSize() {
 }
 
 // Listen for resize events
-// window.addEventListener('resize', adjustCanvasSize);
+window.addEventListener('resize', adjustCanvasSize);
 
 // Initial adjustment
 adjustCanvasSize();
