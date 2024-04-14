920ccbf53bf1d5b1892bdacc47f519a9d4b2b5c8
Dan Ehlers <pinballsurgeon@gmail.com>
2024-04-14 10:47:32 -0400

    dehls -eclipse 2024

--- public/main.js
+++ public/main.js
@@ -138,10 +138,10 @@ async function compareModels() {
         return;
     }
 
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    // tablinks = document.getElementsByClassName("tablinks");
+    // for (i = 0; i < tablinks.length; i++) {
+    //     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // }
 
     document.getElementById('compareTab').style.display = "block";
     evt.currentTarget.className += " active";
